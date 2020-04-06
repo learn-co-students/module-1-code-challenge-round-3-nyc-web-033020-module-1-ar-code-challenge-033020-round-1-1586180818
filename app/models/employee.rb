6603475eab0ea86e214b893f97efc2d59220fa61
github username <github email address>
2020-04-06 10:53:04 -0400

    commit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,13 @@ class Employee < ActiveRecord::Base
     has_many :employee_specialists 
     has_many :specialists, through: :employee_specialists 
     belongs_to :department
+
+    # This method returns the highest number of calls to a specialist. 
+    def self.highest_calls 
+        self.maximum(:specialist_id) 
+    end 
+    # This method returns the employee with the highest number of calls to Specialists. 
+    def self.most_calls 
+        self.where("name = ?", self.highest_calls).first 
+    end 
 end

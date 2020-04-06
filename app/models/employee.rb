1e84599a9bc890be70cd8f6d9f0e73ed862c4174
github username <github email address>
2020-04-06 11:40:42 -0400

    final commit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,7 +5,9 @@ class Employee < ActiveRecord::Base
     has_many :specialists, through: :employee_specialists 
 
     def self.most_calls
-        self.employee_specialists.id
+        es = employee_specialists.all #listint out the joiner method
+        highest = es.select("specialist_id").max #attempting to select the highest number of specialist_id from the joiner method 
+
     end
     #trying to find the appropraite method to return the highest number of calls
 end

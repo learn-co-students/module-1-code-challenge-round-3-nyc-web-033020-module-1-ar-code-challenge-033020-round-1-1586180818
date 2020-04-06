bbddb559a17a432e70623db106af2fd526cc9a82
github username <github email address>
2020-04-06 10:44:40 -0400

    commit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,7 @@
 class Employee < ActiveRecord::Base
     has_many :supervisors 
     has_many :managers, through: :supervisors 
+    has_many :employee_specialists 
+    has_many :specialists, through: :employee_specialists 
     belongs_to :department
 end
